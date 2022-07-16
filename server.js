908a449f2969cc00e20401783b34f8a6575b8c95
mayursarode <sarodemayur55@gmail.com>
2022-07-16 13:13:43 +0530

    Added cors

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ const employee=require("./routes/employee");
 const test=require("./test");
 const verifyToken=require("./middlewares/auth")
 const whitelist = ['http://localhost:3000', 'http://developer2.com']
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
