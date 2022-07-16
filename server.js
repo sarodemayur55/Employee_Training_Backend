505f7760445c12d3938c67731cb4b8b7dd696317
mayursarode <sarodemayur55@gmail.com>
2022-07-16 13:14:43 +0530

    Added cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const whitelist = ['http://localhost:3000', 'http://developer2.com']
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
