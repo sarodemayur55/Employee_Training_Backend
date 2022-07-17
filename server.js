d2ad77524aa4e58e320f728aef05a4d530331469
mayursarode <sarodemayur55@gmail.com>
2022-07-17 19:22:58 +0530

    Handling errors

--- server.js
+++ server.js
@@ -24,8 +24,16 @@ const test=require("./test");
 const verifyToken=require("./middlewares/auth")
 const whitelist = ['http://localhost:3000', 'http://developer2.com','https://ets-9hwzzfgyb-sarodemayur55.vercel.app']
 app.use(function (req, res, next) {
-
+  const corsWhitelist = [
+    'https://domain1.example',
+    'https://domain2.example',
+    'https://domain3.example'
+];
   // Website you wish to allow to connect
+  if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+}
   res.setHeader('Access-Control-Allow-Origin', 'https://ets-9hwzzfgyb-sarodemayur55.vercel.app');
 
   // Request methods you wish to allow
