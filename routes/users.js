6b563cb85b43e61068a9e7b6e85f80d5651f245b
mayursarode <sarodemayur55@gmail.com>
2022-07-19 10:24:39 +0530

    Logout issue

--- routes/users.js
+++ routes/users.js
@@ -120,9 +120,18 @@ router.get("/", auth, async (req, res) => {
 router.get("/logout", async (req, res) => {
   // res.cookie.set('session_token','', { maxAge: 0,
   //   domain: "employee-training-system.vercel.app.somedomain"}).send({ message: "Logged out successfully!" });
-  res
-    .clearCookie("session_token")
-    .send({ message: "Logged out successfully!" });
+
+  res.cookie("session_token", "", {
+    // maxAge: sevenDaysToSeconds,
+    httpOnly: false,
+    Secure:true,
+    expires: new Date(1)
+  }) .send({ message: "Logged out successfully!" });
+
+
+  // res
+  //   .clearCookie("session_token")
+  //   .send({ message: "Logged out successfully!" });
 });
 
 router.patch("/update/:id", async (req, res) => {
